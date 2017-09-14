30235dcd1b510c9bfb84baf427a8b44811e8daa0
Gregory Shimansky <gregory.shimansky@intel.com>
2017-09-14 14:26:34 -0600

    Fixed pktgen command line to always use first 2 ports

--- examples/tutorial/runpktgen.sh
+++ examples/tutorial/runpktgen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ../../dpdk
-sudo ./pktgen -c 0xffffffff -n 4 -- -P -m "[1-2:3-4].0, [5-6:7-8].1, [9-10:11-12].2, [13-14:15-16].3" -T
+sudo ./pktgen -c 0xffffffff -n 4 -- -P -m "[1-2:3-4].0, [5-6:7-8].1" -T
 reset
 
