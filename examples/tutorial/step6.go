9b04ae4957cca114f40aae4ab67a9ed9b81fab2c
Gregory Shimansky <gregory.shimansky@intel.com>
2017-09-14 15:51:57 -0600

    Added step8

--- examples/tutorial/step6.go
+++ examples/tutorial/step6.go
@@ -16,7 +16,6 @@ func main() {
 	firstFlow := flow.SetReceiver(0)
 	secondFlow := flow.SetSeparator(firstFlow, mySeparator, nil)
 	flow.SetHandler(firstFlow, modifyPacket[0], nil)
-	flow.SetHandler(secondFlow, modifyPacket[1], nil)
 	flow.SetSender(firstFlow, 0)
 	flow.SetStopper(secondFlow)
 	flow.SystemStart()
