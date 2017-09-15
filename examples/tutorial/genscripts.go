4bcdb442b2967f093905b400764c293be0fc41e9
Gregory Shimansky <gregory.shimansky@intel.com>
2017-09-15 14:17:20 -0600

    Changed genscripts according to review

--- examples/tutorial/genscripts.go
+++ examples/tutorial/genscripts.go
@@ -7,45 +7,15 @@ import (
 	"os"
 )
 
-func main() {
-	// Parse arguments
-	pktgenDir := flag.String("pgdir", "../../dpdk", "Specify directory where to put pktgen script files")
-	configFile := flag.String("config", "config.json", "Specify config file name")
-	target := flag.String("target", "", "Target host name from config file")
-	pktgen := flag.String("pktgen", "", "Pktgen host name from config file")
-	flag.Parse()
-
-	// Read config
-	err := readConfig(*configFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Write script file for step2
-	step2, err := os.Create(*pktgenDir + string(os.PathSeparator) + "step2.pg")
-    if err != nil {
-		log.Fatal(err)
-	} else {
-		defer step2.Close()
-	}
-
-	fmt.Fprintln(step2, "set 0 dst mac", config[*target][0])
-	fmt.Fprintln(step2, "set 1 dst mac", config[*target][1])
-	fmt.Fprintln(step2, `set 0 size 500
+const (
+	script1 = `set 0 dst mac %s
+set 1 dst mac %s
+set 0 size 500
 set 1 size 500
-`)
-
-	// Write script file for step4
-	step4, err := os.Create(*pktgenDir + string(os.PathSeparator) + "step4.pg")
-    if err != nil {
-		log.Fatal(err)
-	} else {
-		defer step4.Close()
-	}
-
-	fmt.Fprintln(step4, "range 0 dst mac start", config[*target][0])
-	fmt.Fprintln(step4, "range 1 dst mac start", config[*target][1])
-	fmt.Fprintln(step4, `range 0 size start 500
+`
+	script2 = `range 0 dst mac start %s
+range 1 dst mac start %s
+range 0 size start 500
 range 0 size min 500
 range 0 size max 500
 range 0 dst port start 50
@@ -53,19 +23,10 @@ range 0 dst port min 50
 range 0 dst port max 60
 range 0 dst port inc 1
 enable 0 range
-`)
-
-	// Write pktgen script file for step5
-	step5, err := os.Create(*pktgenDir + string(os.PathSeparator) + "step5.pg")
-    if err != nil {
-		log.Fatal(err)
-	} else {
-		defer step5.Close()
-	}
-
-	fmt.Fprintln(step5, "range 0 dst mac start", config[*target][0])
-	fmt.Fprintln(step5, "range 1 dst mac start", config[*target][1])
-	fmt.Fprintln(step5, `range 0 size start 500
+`
+	script3 = `range 0 dst mac start %s
+range 1 dst mac start %s
+range 0 size start 500
 range 0 size min 500
 range 0 size max 500
 range 0 src ip start 111.2.0.0
@@ -73,17 +34,8 @@ range 0 src ip min 111.2.0.0
 range 0 src ip max 111.2.0.3
 range 0 src ip inc 0.0.0.1
 enable 0 range
-`)
-
-	// Write pktgen script for NAT
-	natpg, err := os.Create(*pktgenDir + string(os.PathSeparator) + "nat.pg")
-    if err != nil {
-		log.Fatal(err)
-	} else {
-		defer natpg.Close()
-	}
-
-	fmt.Fprintf(natpg, `range 0 dst mac start %s
+`
+	scriptNat = `range 0 dst mac start %s
 range 1 dst mac start %s
 range 0 size start 500
 range 0 size min 500
@@ -106,17 +58,8 @@ range 1 dst ip inc 0.0.0.0
 range 1 dst port start 1024
 range 1 dst port inc 0
 enable 0 range
-`, config[*target][0], config[*target][1])
-
-	// Write NAT config file
-	natjson, err := os.Create("nat.json")
-    if err != nil {
-		log.Fatal(err)
-	} else {
-		defer natjson.Close()
-	}
-
-	fmt.Fprintf(natjson, `{
+`
+	jsonNat = `{
     "private-port": {
         "index": 0,
         "dst_mac": "%s",
@@ -128,5 +71,53 @@ enable 0 range
         "subnet": "10.1.1.1"
     }
 }
-`, config[*pktgen][0], config[*pktgen][1])
+`
+)
+
+func genScript(hostdir string, hosts []string, format, mac0, mac1 string) {
+	for _, hostname := range hosts {
+		if hostdir != "" {
+			hostname = hostdir + string(os.PathSeparator) + hostname
+		}
+		file, err := os.Create(hostname)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			defer file.Close()
+		}
+
+		fmt.Fprintf(file, format, mac0, mac1)
+	}
+}
+
+func main() {
+	// Parse arguments
+	pktgenDir := flag.String("pgdir", "../../dpdk", "Specify directory where to put pktgen script files")
+	configFile := flag.String("config", "config.json", "Specify config file name")
+	target := flag.String("target", "", "Target host name from config file")
+	pktgen := flag.String("pktgen", "", "Pktgen host name from config file")
+	flag.Parse()
+
+	// Read config
+	err := readConfig(*configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *target == "" && *pktgen == "" {
+		log.Fatal("You must specify either -target or -pktgen parameter")
+	}
+
+	if *target != "" {
+		genScript(*pktgenDir, []string{"step2.pg", "step3.pg"}, script1, config[*target][0], config[*target][1])
+		genScript(*pktgenDir, []string{"step4.pg"}, script2, config[*target][0], config[*target][1])
+		genScript(*pktgenDir, []string{"step5.pg", "step6.pg", "step7.pg", "step8.pg", "step9.pg", "step10.pg", "step11.pg"},
+			script3, config[*target][0], config[*target][1])
+		genScript(*pktgenDir, []string{"nat.pg"}, scriptNat, config[*target][0], config[*target][1])
+	}
+
+	if *pktgen != "" {
+		// Write NAT config file
+		genScript("", []string{"nat.json"}, jsonNat, config[*pktgen][0], config[*pktgen][1])
+	}
 }
